c8109ae015b7d1709b7f63384218c8133d7ef553
Matei Garila <mateigarila@hotmail.com>
2022-10-24 13:53:47 -0400

    Added more folders to better structure the project

--- SOEN341/includes/dbh.inc.php
+++ SOEN341/includes/dbh.inc.php
@@ -3,6 +3,10 @@
 $serverName = "localhost";
 $dBUsername = "root";
 $dBPassword = "";
-$dBName = "";
+$dBName = "soen341";
 
-$conn = mysqli_connect($serverName, $dBUsername,);
+$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
+
+if(!$conn){
+	die("Connection Failed: " . mysqli_connect_error());
+}
--- SOEN341/login.php
+++ SOEN341/login.php
@@ -5,12 +5,9 @@
 	<section>
 		<h2>Log in</h2>
 		<form action="login.inc.php" method="post">	
-			<input type="text" name="name" placeholder="Your full name...">
-			<input type="text" name="email" placeholder="Your email...">
-			<input type="text" name="uid" placeholder="Your username...">
+			<input type="text" name="name" placeholder="Your username/email...">
 			<input type="password" name="pwd" placeholder="Your password...">
-			<input type="password" name="pwdRepeat" placeholder="Confirm password...">
-			<button type="submit" name="submit">Sign up!</button>
+			<button type="submit" name="submit">Log in!</button>
 		</form>		
 	</section>
 
